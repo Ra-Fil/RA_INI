2b59b5640686176140d48a42fb4d8b24c95f8718
Ra-Fil <r.filipova@email.cz>
2025-01-07 00:10:49 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,14 +1,13 @@
-CORS(app)
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app
 
-from flask import Flask, request, jsonify, send_file
 from bs4 import BeautifulSoup
 import requests
 import json
 import os
-
-from flask_cors import CORS
 
 @app.route('/scrape', methods=['POST'])
 def scrape_google():
