ae3ef9323e91f72c4d974c086dfec5620cc80df9
Ra-Fil <r.filipova@email.cz>
2025-01-06 23:04:20 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -39,6 +39,7 @@ def scrape_google():
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(results, f, ensure_ascii=False, indent=4)
 
+    return send_file(file_path, as_attachment=True)
 
     import os
 
@@ -46,8 +47,6 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
 
-
-    return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
