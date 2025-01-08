b5903e4c4fa63931756c25e9c2fab38b10264316
Ra-Fil <r.filipova@email.cz>
2025-01-08 22:14:02 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify, send_file
-from flask_cors import CORS 
-
-app = Flask(__name__)
-CORS(app)
-
+from flask_cors import CORS
 from bs4 import BeautifulSoup
 import requests
 import csv
 import json
 import os
+
+app = Flask(__name__)
+
+CORS(app, origins=["https://ra-fil.github.io"])
 
 @app.route('/', methods=['GET'])
 def home():
@@ -34,32 +34,23 @@ def scrape_google():
     soup = BeautifulSoup(response.text, 'html.parser')
     results = []
 
-for g in soup.find_all('div', class_='tF2Cxc'):
-    title = g.find('h3').text if g.find('h3') else 'No title'
-    link = g.find('a')['href'] if g.find('a') else 'No link'
-    
-    description_element = g.find('span', class_='aCOpRe')
-    meta_description = description_element.text if description_element else 'No meta description'
-    
-    results.append({
-        'title': title,
-        'link': link,
-        'meta_description': meta_description
-    })
-
+    # Cyklus pro extrakci výsledků
+    for g in soup.find_all('div', class_='tF2Cxc'):
+        title = g.find('h3').text if g.find('h3') else 'No title'
+        link = g.find('a')['href'] if g.find('a') else 'No link'
         
-    
+        description_element = g.find('span', class_='aCOpRe')
+        meta_description = description_element.text if description_element else 'No meta description'
+        
+        results.append({
+            'title': title,
+            'link': link,
+            'meta_description': meta_description
+        })
+
     # Vytvoření CSV souboru
     file_path = 'results.csv'
     with open(file_path, 'w', encoding='utf-8', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['Title', 'Link', 'Meta Description'])
-        for result in results:
-            writer.writerow([result['title'], result['link'], result['meta_description']])
-
-    return send_file(file_path, as_attachment=True)
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-
+        for result in re
