aee4ada28abb31cc70e3989e725498d244321784
Ra-Fil <r.filipova@email.cz>
2025-01-10 13:27:07 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -61,5 +61,3 @@ def scrape_google():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-
-response = requests.get(url, headers=headers, timeout=5)
